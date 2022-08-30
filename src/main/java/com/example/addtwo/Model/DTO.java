fee112932c3df5760b1eeabb5f5ee9fcccb58675
FHallengreen <fred70p9@stud.kea.dk>
2022-08-30 13:09:02 +0200

    SpringBootWeb

---
+++ src/main/java/com/example/addtwo/Model/DTO.java
@@ -0,0 +1,32 @@
+package com.example.addtwo.Model;
+
+public class DTO {
+    private String value_1;
+    private String value_2;
+
+    public DTO(String value_1, String value_2) {
+        this.value_1 = value_1;
+        this.value_2 = value_2;
+    }
+
+    public String getValue_1() {
+        return value_1;
+    }
+
+    public void setValue_1(String value_1) {
+        this.value_1 = value_1;
+    }
+
+    public String getValue_2() {
+        return value_2;
+    }
+
+    public void setValue_2(String value_2) {
+        this.value_2 = value_2;
+    }
+
+    public String Total(){
+        return value_1 + value_2;
+    }
+
+}
---
+++ src/test/java/com/example/addtwo/AddTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
